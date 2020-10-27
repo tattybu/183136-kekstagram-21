05d98dc1fa3a75b85c5a281d76bb86e34f125093
tattybu <tatty_bu@mail.ru>
2020-10-27 21:06:59 +0200

    Фото грузятся с удалённого сервера

---
+++ js/load.js
@@ -0,0 +1,39 @@
+'use strict';
+
+(function () {
+
+  const getData = (onSuccess, onError) => {
+
+    let URL = `https://21.javascript.pages.academy/kekstagram/data`;
+    let xhr = new XMLHttpRequest();
+
+    xhr.responseType = `json`;
+
+    xhr.addEventListener(`load`, function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener(`error`, function () {
+      onError(`Ошибка соединения`);
+    });
+
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open(`GET`, URL);
+    xhr.send();
+  };
+
+
+  window.load = {
+    getData
+  };
+
+})();
